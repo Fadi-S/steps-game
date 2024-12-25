a24c03e368aa33bc457e99f9d9a30b9707f6b95b
Fadi Sarwat <fadisarwat@yahoo.com>
2024-12-25 21:51:03 +0200

    Fix imports

--- src/components/question_modal.tsx
+++ src/components/question_modal.tsx
@@ -1,7 +1,9 @@
 import { Dialog, DialogBackdrop, DialogPanel, DialogTitle } from "@headlessui/react"
 import { CheckIcon } from "@heroicons/react/24/outline"
-import { Option, Question, QuestionType } from "../features/quizzes/quizzesApiSlice"
-import React, { useState } from "react"
+import { QuestionType } from "../features/quizzes/quizzesApiSlice"
+import type { Question } from "../features/quizzes/quizzesApiSlice"
+import { useState } from "react"
+import type React from "react"
 import { XMarkIcon } from "@heroicons/react/24/solid"
 
 interface ModalProps {
